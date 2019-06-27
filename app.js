0fb10b014981f7e362a3c67f504ceec410ccb532
felipemuner <felipe.muner@gmail.com>
2019-06-27 11:56:40 +0700

    just cleaning app.js

--- app.js
+++ app.js
@@ -36,9 +36,6 @@ app.use((err, req, res, next) => {
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  console.log(res.locals);
-
   res.status(err.status || 500);
   res.json({
     message: res.locals.message,
